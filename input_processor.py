06d2eb27121efbb5769e4ff094926be7b288dcd0
Tyler <5478568+Tyler-Yates@users.noreply.github.com>
2020-07-04 20:56:01 -0500

    Add method for minimum word length

--- input_processor.py
+++ input_processor.py
@@ -11,12 +11,16 @@ def _is_normal_word(word: str, case_sensitive=True) -> bool:
     return match is None
 
 
+def _is_at_least_length(word: str, length=3) -> bool:
+    return len(word) >= length
+
+
 def main():
     with open("input.txt", mode='r') as input_file:
         for line in input_file.readlines():
             line = line.strip()
             word = line.split('/')[0]
-            if _is_normal_word(word):
+            if _is_normal_word(word) and _is_at_least_length(word, 3):
                 print(word)
 
 
